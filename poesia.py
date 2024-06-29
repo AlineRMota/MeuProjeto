80c7b09f4871f28430c7a84f2b6e91e08b7ef469
Aline Rodrigues Mota <149389028+AlineRMota@users.noreply.github.com>
2024-06-29 15:00:10 -0300

    poesia.py

--- poesia.py
+++ poesia.py
@@ -3,3 +3,18 @@ print("De tudo, ao meu amor serei atento")
 print("Antes, e com tal zelo, e sempre, e tanto")
 print("Que mesmo em face do maior encanto")
 print("Dele se encante mais meu pensamento")
+
+print("Quero vivê-lo em cada vão momento")
+print("E em seu louvor hei de espalhar meu canto")
+print("E rir meu riso e derramar meu pranto")
+print("Ao seu pesar ou seu contentamento")
+
+print("E assim quando mais tarde me procure")
+print("Quem sabe a morte, angústia de quem vive")
+print("Quem sabe a solidão, fim de quem ama")
+
+print("Eu possa lhe dizer do amor (que tive):")
+print("Que não seja imortal, posto que é chama")
+print("Mas que seja infinito enquanto dure")
+
+print("Vinícius de Moraes")
